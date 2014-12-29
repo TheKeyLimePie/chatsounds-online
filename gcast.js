cdc10a7ca08620497843d21efd078f017e294161
KeyLimePie <v.k.asakura@gmail.com>
2014-12-29 21:36:38 +0100

    Got rid of Google Chromecast

--- gcast.js
+++
@@ -1,6 +0,0 @@
-
-$("#headbar_cast").on("click", function ()
-{
-	var notiCast = new Notification ("Google Chromecast", "We're working on Google Chromecast support.<br />Stay tuned and (soon) happy casting!<br />ETA: I don't know...", true, 2);
-	notiCast.init ();
-});
